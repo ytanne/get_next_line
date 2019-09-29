a57838a4bdaa41bd844693ce4c1687d1539747ee
Yerzhan Orazayev <yorazaye@e1z3r6p23.42.us.org>
2019-09-28 22:27:18 -0700

    Modified the function to stop on new line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yorazaye <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/27 07:45:38 by yorazaye          #+#    #+#             */
-/*   Updated: 2019/09/28 20:44:02 by yorazaye         ###   ########.fr       */
+/*   Updated: 2019/09/28 22:22:50 by yorazaye         ###   ########.fr       */
 /*                                                                           */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@
 #include "libft/libft.h"
 #include <string.h>
 
-/*
-static size_t	is_char_there(char *str, char c)
+static size_t	is_ch_th(char *str, char c)
 {
 	size_t	i;
 
@@ -28,22 +27,29 @@ static size_t	is_char_there(char *str, char c)
 	}
 	return (i);
 }
-*/
 
 int		get_next_line(int fd, char **line)
 {
-	static int	bytes[2];
-	char		tmp[BUFF_SIZE + 1];
-	char		*ptr;
+	static size_t	bytes[2];
+	char			tmp[BUFF_SIZE + 1];
+	char			*ptr;
 
 	bytes[1] = 0;
 	while ((bytes[0] = read(fd, tmp, BUFF_SIZE)))
 	{
-		bytes[1] += bytes[0];
+		if (is_ch_th(tmp, '\n') != ft_strlen(tmp))
+			bytes[1] += is_ch_th(tmp, '\n');
+		else
+			bytes[1] += bytes[0];
 		ptr = (*line);
 		(*line) = ft_strjoin(ptr, tmp);
 		(*line)[bytes[1]] = '\0';
 		ft_strclr(ptr);
+		if (bytes[0] != is_ch_th(tmp, '\n'))
+		{
+			ft_strclr(tmp);
+			break ;
+		}
 		ft_strclr(tmp);
 	}
 	ft_putstr(*line);
@@ -62,11 +68,13 @@ int		main(int ac, char **av)
 	int		rc;
 	char	*r;
 
-	r = (char *)malloc(sizeof(char)*100);
 	i = 1;
+	r = ft_strnew(1);
 	if (ac >= 2)
 	{
 		fd = open(av[i], O_RDONLY);
+		rc = get_next_line(fd, &r);
+		ft_putchar('\n');
 		rc = get_next_line(fd, &r);
 		close(fd);
 		//printf("%s\n", ft_strchr("Hello\nWorld", '\n'));
