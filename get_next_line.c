f990100854ab9c0ffd2a374d3dbd47c8532236ea
Yerzhan Orazayev <yorazaye@e1z3r6p23.42.us.org>
2019-09-28 20:45:19 -0700

    Updated code

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,48 @@
 /*   By: yorazaye <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/27 07:45:38 by yorazaye          #+#    #+#             */
-/*   Updated: 2019/09/27 10:47:32 by yorazaye         ###   ########.fr       */
-/*                                                                            */
+/*   Updated: 2019/09/28 20:44:02 by yorazaye         ###   ########.fr       */
+/*                                                                           */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "libft/libft.h"
+#include <string.h>
 
-static size_t bytes;
+/*
+static size_t	is_char_there(char *str, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			break ;
+		i++;
+	}
+	return (i);
+}
+*/
 
 int		get_next_line(int fd, char **line)
 {
-	bytes = read(fd, *line, BUFF_SIZE);
-	return (bytes);
+	static int	bytes[2];
+	char		tmp[BUFF_SIZE + 1];
+	char		*ptr;
+
+	bytes[1] = 0;
+	while ((bytes[0] = read(fd, tmp, BUFF_SIZE)))
+	{
+		bytes[1] += bytes[0];
+		ptr = (*line);
+		(*line) = ft_strjoin(ptr, tmp);
+		(*line)[bytes[1]] = '\0';
+		ft_strclr(ptr);
+		ft_strclr(tmp);
+	}
+	ft_putstr(*line);
+	return (bytes[0]);
 }
 
 #ifdef TEST
@@ -33,14 +62,15 @@ int		main(int ac, char **av)
 	int		rc;
 	char	*r;
 
+	r = (char *)malloc(sizeof(char)*100);
 	i = 1;
 	if (ac >= 2)
 	{
-		r = (char *)malloc(sizeof(char) * 100);
 		fd = open(av[i], O_RDONLY);
 		rc = get_next_line(fd, &r);
 		close(fd);
-		printf("%s\n", r);
+		//printf("%s\n", ft_strchr("Hello\nWorld", '\n'));
+		//printf("%s\n", r);
 	}
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: yorazaye <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/27 07:37:54 by yorazaye          #+#    #+#             */
-/*   Updated: 2019/09/27 09:44:47 by yorazaye         ###   ########.fr       */
+/*   Updated: 2019/09/28 20:43:27 by yorazaye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@
 #include <sys/uio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 int		get_next_line(const int fd, char **line);
 
-# define BUFF_SIZE 32
+# define BUFF_SIZE 3
 
 #endif
