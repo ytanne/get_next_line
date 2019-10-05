b373d07fc862cc40e9eb768e79082a391f2fc51f
Yerzhan Orazayev <yorazaye@e1z3r6p23.42.us.org>
2019-10-04 23:02:39 -0700

    6 out of 9 tests are passed

--- get_next_line.c
+++ get_next_line.c
@@ -6,56 +6,69 @@
 /*   By: yorazaye <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/02 20:45:20 by yorazaye          #+#    #+#             */
-/*   Updated: 2019/10/03 12:29:36 by yorazaye         ###   ########.fr       */
+/*   Updated: 2019/10/04 22:59:59 by yorazaye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "get_next_line.h"
 
-int		init(char **line, char *tmp)
+void	deal_with_ptr(char *ptr)
 {
+	char	*store;
 	int		i;
-	char	*ptr[2];
-	int		r;
 
 	i = 0;
-	r = 0;
-	while (tmp[i] != '\n' && tmp[i] != '\0')
-		i++;
-	if (i >= 0 && i < (int)ft_strlen(tmp))
-		r = 1;
-	ptr[0] = ft_strsub(tmp, 0, i);
-	ptr[1] = *line;
-	*line = ft_strjoin(ptr[1], ptr[0]);
-	free(ptr[0]);
-	if (*ptr[1])
-		free(ptr[1]);
-	return (r);
+	store = ft_strchr(ptr, '\n') + 1;
+	i = ft_strlen(store);
+	ptr = ft_memmove(ptr, store, i);
+	ptr[i] = '\0';
 }
 
-void	get_upto_nl(char *ptr, char **line)
+int		init(char **line, char *ptr)
 {
 	int		i;
+	char	*tmp[2];
 
 	i = 0;
-	while (ptr[i] != '\0' && ptr[i] != '\n')
+	while (ptr[i] != '\n' && ptr[i] != '\0')
 		i++;
-	if (i == BUFF_SIZE)
-		i = 0;
-	*line = ft_strdup(ft_strchr(ptr, '\n') + 1);
+	tmp[0] = ft_strsub(ptr, 0, i);
+	tmp[1] = *line;
+	*line = ft_strdup(tmp[0]);
+	ft_memdel((void **)&tmp[0]);
+	if (tmp[1])
+		ft_memdel((void **)&tmp[1]);
+	if (ptr[i] != '\n')
+	{
+		ptr[0] = '\0';
+		return (0);
+	}
+	deal_with_ptr(ptr);
+	return (1);
+}
+
+int		get_upto_nl(char *ptr, char **line)
+{
+	int		r;
+
+	r = 0;
+	if (init(line, ptr) == 1 || **line)
+		r = 1;
+	return (r);
 }
 
 int		get_next_line(const int fd, char **line)
 {
 	int			byte;
 	char		tmp[BUFF_SIZE + 1];
-	static char ptr[FD_SIZE][BUFF_SIZE + 1] = {[0 ... (FD_SIZE - 1)] = {0}};
+	static char ptr[FD_SIZE][BUFF_SIZE + 1];
 
 	if (*line)
 		ft_strclr(*line);
 	if (*ptr[fd])
-		get_upto_nl(ptr[fd], line);
+		if (get_upto_nl(ptr[fd], line) == 1)
+			return (1);
 	while ((byte = read(fd, tmp, BUFF_SIZE)) > 0)
 	{
 		tmp[byte] = '\0';
@@ -67,33 +80,8 @@ int		get_next_line(const int fd, char **line)
 	}
 	if (byte < 0)
 		return (-1);
-	else if (byte < BUFF_SIZE)
-		return (0);
-	else
+	else if (byte != 0 || *ptr[fd] || **line)
 		return (1);
-}
-
-int		main(void)
-{
-	int		fd;
-	char	*line;
-
-	fd = open("caesar_quote.txt", O_RDONLY);
-	if (fd >= 0)
-	{
-		ft_putstr("Result of get_next_line is: ");
-		ft_putnbr(get_next_line(fd, &line));
-		ft_putchar('\n');
-		ft_putstr("--------------------\n");
-		ft_putstr(line);
-		ft_putstr("\n--------------------\n");
-		ft_putstr("Result of get_next_line is: ");
-		ft_putnbr(get_next_line(fd, &line));
-		ft_putchar('\n');
-		ft_putstr("--------------------\n");
-		ft_putstr(line);
-	}
 	else
-		ft_putnbr(fd);
-	return (0);
+		return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,19 @@
 /*   By: yorazaye <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/27 07:37:54 by yorazaye          #+#    #+#             */
-/*   Updated: 2019/10/03 12:30:22 by yorazaye         ###   ########.fr       */
+/*   Updated: 2019/10/03 18:34:10 by yorazaye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
+# define GET_NEXT_LINE_H
+
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
 int		get_next_line(const int fd, char **line);
 
